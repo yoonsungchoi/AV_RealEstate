146dee313f78353ed0083a3246fdc6c89712b041
younsung <kisp1049@gmail.com>
2017-06-03 23:15:07 +0900

    Merge branch 'master' of C:\github_repo\AVRealEstate\AV_RealEstate with conflicts.

--- app/src/main/java/com/example/lee/loading_page_2/action/FavoriteActivity.java
+++ app/src/main/java/com/example/lee/loading_page_2/action/FavoriteActivity.java
@@ -3,7 +3,6 @@ package com.example.lee.loading_page_2.action;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
